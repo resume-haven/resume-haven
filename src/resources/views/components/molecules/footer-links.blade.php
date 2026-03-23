40b0a1e31ea3d5b10cec6cff054e6890e6eabd90
Guido Faust <gfaust@digifa.de>
2026-03-23 13:52:54 +0100

    feat(ui): extract footer links into FooterLinks molecule for reusability

---
+++ src/resources/views/components/molecules/footer-links.blade.php
@@ -0,0 +1,8 @@
+{{-- Molecule: Footer-Links (Atomic Design) --}}
+<ul class="flex flex-col items-center gap-2 sm:flex-row sm:gap-6 sm:justify-end sm:w-auto sm:ml-auto">
+    <li><x-atoms.link href="/impressum">Impressum</x-atoms.link></li>
+    <li><x-atoms.link href="/datenschutz">Datenschutz</x-atoms.link></li>
+    <li><x-atoms.link href="/kontakt">Kontakt</x-atoms.link></li>
+    <li><x-atoms.link href="/lizenzen">Lizenzen</x-atoms.link></li>
+</ul>
+
